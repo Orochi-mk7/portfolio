153267316195d96c2386b83acf87064d073f9015
Mohammed Al Khayyat <oroc.oroc.mohamad@gmail.com>
2025-08-05 20:25:16 +0300

    fixed react-smooth

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),svgr()],
+  plugins: [react(),tailwindcss(),svgr()], 
+  optimizeDeps: {
+    exclude: ['react-smooth'],  // exclude from dependency pre-bundling
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-smooth'],  // mark as external to not bundle it
+    }}
+  
 })
